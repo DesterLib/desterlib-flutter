56965b9b86b87c80b0e18ad5fea127870ac62217
AlkenD <desteralken@gmail.com>
2025-10-15 04:17:07 +0530

    fix: custom router fade transitions for pages

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,22 @@ import 'package:media_kit/media_kit.dart';
 import 'package:window_manager/window_manager.dart';
 import 'web_view_page.dart';
 
+// Custom page transition builder for pure fade effect
+class FadePageTransitionsBuilder extends PageTransitionsBuilder {
+  const FadePageTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return FadeTransition(opacity: animation, child: child);
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Initialize media_kit
@@ -32,9 +48,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WebViewPage(),
+      theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: FadePageTransitionsBuilder(),
+            TargetPlatform.iOS: FadePageTransitionsBuilder(),
+            TargetPlatform.linux: FadePageTransitionsBuilder(),
+            TargetPlatform.macOS: FadePageTransitionsBuilder(),
+            TargetPlatform.windows: FadePageTransitionsBuilder(),
+          },
+        ),
+      ),
+      home: const WebViewPage(),
     );
   }
 }
